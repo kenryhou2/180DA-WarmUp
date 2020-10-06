df5ebdf8e7002815a48444567081455bcc910979
kenryhou2 <kouhenry@yahoo.com>
2020-10-06 13:23:52 -0700

    updated dominant color

--- dom_color_k_means.py
+++ dom_color_k_means.py
@@ -49,12 +49,21 @@ width = int (cap.get(3))
 while(1):   
     _, frame = cap.read() 
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
-
+    target_upper_left = (((width//2)-100),((height//2)-100))
+    target_lower_right = (((width//2)+100),((height//2)+100))
+    #print("left",target_upper_left,"right:", target_lower_right)
+    target_box = cv2.rectangle(frame,target_upper_left,target_lower_right,(255,0,0),3)
+    target_area = img[target_upper_left[1]:target_lower_right[1], target_upper_left[0]:target_lower_right[0]] #y1:y2,x1:x2 where y is lower_right, and x is upper left
+    
+    
+    #cv2.imshow('target area', target_area) #inverted image?? but the color values are okay.
+    #grab area to perform the kmeans process and reshape it
+    #img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
+    target_area = target_area.reshape((target_area.shape[0] * target_area.shape[1],3))
     cluster_num = 1 #cluster number indicates what's the k dominant/majority colors in the image
     clt = KMeans(n_clusters=cluster_num) #cluster number
-    clt.fit(img)
-
+    #clt.fit(img)
+    clt.fit(target_area)
     hist = find_histogram(clt)
     color = plot_colors2(hist, clt.cluster_centers_) #grab dominant color
     #bar = plot_colors2(hist, clt.cluster_centers_)
@@ -63,7 +72,7 @@ while(1):
 
     #draw color in a box
     #print(color)
-s
+
     bgr_color = (color[2], color[1], color[0])
     #bgr_color = (0,255,0)
 
